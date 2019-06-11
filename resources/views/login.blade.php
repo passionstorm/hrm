7350a2774b763d5aa0461d7ddfd0c775bc377342
minhvh <minhvh@soft-flight.work>
2019-06-11 13:19:24 +0900

    change title form login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../../index2.html"><b>Human Resource Manager</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
