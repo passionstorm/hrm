55635dc1a8a5badaf89909d641047f34b078fdc6
nguyen van nhan <nvnit1176211@gmail.com>
2019-08-02 15:15:08 +0700

    remaked project list

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -15,10 +15,12 @@ class Role
      */
     public function handle($request, Closure $next, ...$roles)
     {
+        if( !Auth::check() ){
+            return redirect('login')->with('fail', 'You must login!');
+        }
         if (Auth::check() && in_array(Auth::user()->role, $roles)) {
             return $next($request);
         }
-
         abort(404);
     }
 }
